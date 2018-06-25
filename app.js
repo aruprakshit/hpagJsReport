9e3b778a868201d803e838923c29de06dc238ca1
Arup Rakshit <aruprakshit@rocketmail.com>
2018-06-25 16:30:53 +0530

    fix style

--- app.js
+++ app.js
@@ -3,91 +3,26 @@ fetch("http://127.0.0.1:3002/db.json")
     return response.json();
   })
   .then(function(reportdata) {
-    print(reportdata[1].component);
+    print(reportdata);
   });
 
 function print(reportData) {
   const docDefinition = {
-    content: [
-      { text: reportData.name },
-      { text: "Checklist Items:", style: "header" },
-      {
-        ul: checklists(reportData.checklistItems)
-      },
-      { text: "Guidance :", style: "header" },
-      { text: reportData.guidance },
-      { text: "Report Text :", style: "header" },
-      { text: reportData.reporttext },
-      { text: "Findings/Deficiencies :", style: "header" },
-      { text: reportData.findings },
-      {
-        columns: [
-          { text: "Status :", width: "32%" },
-          { text: "Responsibility :", width: "32%" },
-          [
-            { text: `Due Date : ${reportData.duedate}` },
-            { text: `Closing Date : ${reportData.closingdate}` }
-          ]
-        ]
-      },
-      { text: "Attachments :" },
-      {
-        columns: [
-          [
-            {
-              image: reportData.attachments[0].url,
-              width: 150
-            },
-            {
-              columns: [
-                reportData.attachments[0].name,
-                {
-                  text: "View >>",
-                  link: reportData.attachments[0].url,
-                  decoration: "underline"
-                }
-              ]
-            }
-          ],
-          [
-            {
-              image: reportData.attachments[1].url,
-              width: 150
-            },
-            {
-              columns: [
-                reportData.attachments[1].name,
-                {
-                  text: "View >>",
-                  link: reportData.attachments[1].url,
-                  decoration: "underline"
-                }
-              ]
-            }
-          ],
-          [
-            {
-              image: reportData.attachments[2].url,
-              width: 150
-            },
-            {
-              columns: [
-                reportData.attachments[2].name,
-                {
-                  text: "View >>",
-                  link: reportData.attachments[2].url,
-                  decoration: "underline"
-                }
-              ]
-            }
-          ]
-        ]
-      }
-    ],
+    content: [...componentsBody(reportData)],
     styles: {
-      header: {
-        bold: true,
-        fontsize: 15
+      headerOne: {
+        fontsize: 14,
+        margin: [0, 10, 0, 10]
+      },
+      headerTwo: {
+        fontsize: 12,
+        margin: [0, 5, 0, 5]
+      },
+      global: {
+        fontsize: 5
+      },
+      image: {
+        margin: [0, 0, 0, 7]
       }
     }
   };
@@ -104,4 +39,102 @@ function checklists(data) {
       ]
     };
   });
+}
+
+function componentsBody(components) {
+  return components.map(({ component }) => [
+    { text: component.name, style: "headerOne" },
+    { text: "Checklist Items:", style: "headerTwo" },
+    {
+      ul: checklists(component.checklistItems)
+    },
+    { text: "Guidance :", style: "headerTwo" },
+    { text: component.guidance, style: ["global"], margin: [0, 0, 0, 5] },
+    { text: "Report Text :", style: "headerTwo" },
+    { text: component.reporttext, style: ["global"], margin: [0, 0, 0, 5] },
+    { text: "Findings/Deficiencies :", style: "headerTwo" },
+    { text: component.findings, style: ["global"], margin: [0, 0, 0, 5] },
+    {
+      columns: [
+        [
+          { text: "Status :", width: "32%", style: "headerTwo" },
+          { text: component.status, width: "32%", style: "global" }
+        ],
+        [
+          { text: "Responsibility :", width: "32%", style: "headerTwo" },
+          { text: component.responsibility, width: "32%", style: "global" }
+        ],
+        [
+          {
+            columns: [
+              { text: "Due Date :", style: "headerTwo" },
+              { text: component.duedate, style: "global" }
+            ]
+          },
+          {
+            columns: [
+              { text: "Closing Date :", style: "headerTwo" },
+              { text: component.closingdate, style: "global" }
+            ]
+          }
+        ]
+      ]
+    },
+    { text: "Attachments :", style: "headerTwo"},
+    {
+      columns: [
+        [
+          {
+            image: component.attachments[0].url,
+            width: 150,
+            style: 'image'
+          },
+          {
+            columns: [
+              component.attachments[0].name,
+              {
+                text: "View >>",
+                link: component.attachments[0].url,
+                decoration: "underline"
+              }
+            ]
+          }
+        ],
+        [
+          {
+            image: component.attachments[1].url,
+            width: 150,
+            style: 'image'
+          },
+          {
+            columns: [
+              component.attachments[1].name,
+              {
+                text: "View >>",
+                link: component.attachments[1].url,
+                decoration: "underline"
+              }
+            ]
+          }
+        ],
+        [
+          {
+            image: component.attachments[2].url,
+            width: 150,
+            style: 'image'
+          },
+          {
+            columns: [
+              component.attachments[2].name,
+              {
+                text: "View >>",
+                link: component.attachments[2].url,
+                decoration: "underline"
+              }
+            ]
+          }
+        ]
+      ]
+    }
+  ]);
 }
